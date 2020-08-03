715bab25ac1de8f42b961fc380d26480405618dd
Thykof <thykof@protonmail.ch>
2020-08-03 08:43:57 +0200

    Add connectors

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 
 import App from './App';
+import Connection from './connection/Connection'
 import './index.css';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root')
+function getLibrary(provider) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 8000;
+  return library;
+}
+
+ReactDOM.render((
+	<Web3ReactProvider getLibrary={getLibrary}>
+    <Connection />
+		<App />
+	</Web3ReactProvider>
+), document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+serviceWorker.register();
--- app/src/store/drizzleOptions.js
+++ app/src/store/drizzleOptions.js
@@ -1,8 +1,18 @@
 // let drizzle know what contracts we want and how to access our test blockchain
-// import Web3 from "web3";
 
 import Logistic from "../contracts/Logistic.json";
 import { EVENT_NAMES } from "./constants";
+
+// import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
+// import { InjectedConnector } from '@web3-react/injected-connector'
+
+// import Web3 from "web3";
+// const HDWalletProvider = require("@truffle/hdwallet-provider");
+// const mnemonic = "finger retreat rude front release kid equal swear sibling habit beyond dwarf"
+// const host = "http://13762fdc2f1f.ngrok.io"
+// const http = "http://localhost:8545"
+// const ws = "ws://localhost:8545"
+
 
 const options = {
   contracts: [Logistic],
@@ -17,7 +27,10 @@ const options = {
   },
   // web3: {
   //   block: false,
-  //   customProvider: new Web3("ws://localhost:8545"),
+  //   // customProvider: new Web3(ws),
+  //   // customProvider: new Web3(host),
+  //   // customProvider: new Web3(new HDWalletProvider(mnemonic, host)),
+  //   // customProvider: new Web3.providers.HttpProvider(host),
   // },
 };
 
